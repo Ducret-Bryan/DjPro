f66b21c413f0b21cd011834339d411f171aa8f92
Ducret Bryan <ducret.bryan@gmail.com>
2024-05-04 13:07:54 +0200

    feat: add navbar

---
+++ app/src/view/components/navbar.php
@@ -0,0 +1,17 @@
+<nav>
+    <div class="container">
+        <img id="logo" src="./asset/image/logo.png" alt="logo" />
+        <button type="button" id="burger_menu">
+            <img src="./asset/image/burger.svg" alt="icons burger" />
+        </button>
+        <div id="navLink" class="hidden">
+            <a href="#">Accueil</a>
+            <a href="#">Nos critères</a>
+            <a href="#">Nos Djs</a>
+            <a href="#">Nos Evénements</a>
+            <a href="#">Galerie</a>
+            <a href="#">Location</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+</nav>
