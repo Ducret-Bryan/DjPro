3aa167bc46f430063fbb0b4aa779daac0b61f356
Ducret Bryan <ducret.bryan@gmail.com>
2024-05-04 12:49:00 +0200

    Dockerisation

---
+++ app/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo 'Navbar' ?>
+    <header>
+    </header>
+    <main>
+        <?php echo 'Page_content' ?>
+    </main>
+    <?php echo 'Footer' ?>
+</body>
+
+</html>
