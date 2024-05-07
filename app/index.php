c19b2d54e3d99720a7f97a09e4d7d6eeda3854a0
Ducret Bryan <ducret.bryan@gmail.com>
2024-05-07 14:56:18 +0200

    feat: add criteria page

--- app/index.php
+++ app/index.php
@@ -1,8 +1,18 @@
 <?php
 require_once('src/controller/homeController.php');
+require_once('src/controller/criteriaController.php');
 
 try {
-    if (isset($_GET['action']) && $_GET['action'] == '') {
+    if (isset($_GET['action']) && !$_GET['action'] == '') {
+        $action = $_GET['action'];
+
+        switch ($action) {
+            case 'criteria':
+                criteriaPage();
+                break;
+            default:
+                header('Location: /');
+        }
     } else {
         homePage();
     }
---
+++ app/src/view/pages/criteria.php
@@ -0,0 +1,64 @@
+<h1>
+    Nos Critères
+</h1>
+<p id="subTitle">
+    Comment reconnaitre un véritable DJ professionnel.
+</p>
+
+<section>
+    <p>Sur quel critères nous avons sélectionné les meilleurs ? Lisez-nous et vous serez rapidement convaincu du sérieux de ce site.</p>
+    <article>
+        <h2>Critère n°1</h2>
+        <p>
+            Généralement un DJ professionnel travaille individuellement et à sa propre entreprise avec de ce fait un numéro de Siret. Cela n’est bien entendu pas la seule référence mais c’est le cas de tous les DJ qui vous seront proposés.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°2</h2>
+        <p>
+            il détient obligatoirement son propre matériel de sonorisation et d’éclairage professionnelle. Son installation doit être propre et parfaitement sécurisé. C’est le cas de tous les DJ présent sur ce site.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°3</h2>
+        <p>
+            Il doit être capable d’enchaîner les titres mais ils doit aussi être capable de prendre le micro et booster sans être lourd. Être capable d’assurer un jeu lors dès mariage plutôt que d’être statique derrière sa console est un gros plus pour l’ambiance.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°4</h2>
+        <p>
+            Un DJ doit tout comme un artisan avoir une assurance responsabilité civile. Imaginons qu’il viendrait à dégrader la salle que vous avez loué et qu’il n’a pas d’assurance et bien c’est vous qui paierai. Il est de ce fait préférable pour vous de penser à vérifier ce point souvent négligé. Ce n’est pas le cas ici.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°5</h2>
+        <p>
+            Votre DJ à son véhicule utilitaire de société et généralement cela n’est pas un Kangoo 😁. Bien entendu cela dépend du volume de la salle et du nombre de convives à votre événement. Cela paraît Bénin de vérifier ce détail mais si votre DJ vient en twingo 😁, il y a un problème 😉.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°6</h2>
+        <p>
+            Quel tarif pour un DJ professionnel ? Vous voyez tout et n’importe quoi généralement sur ce point et de ce fait vous êtes dans le flou normal ! Combien 350 ? 600 ? 1000€ ? et même plus visible sur le net. Facile pour nous de répondre à cette question. Sachant qu’un Pro à un coup de fonctionnement comme décrit ci-dessus à 350€ ce n’est pas un professionnel mais plutôt le DJ du coin de votre rue et là vous vous exposez à une soirée gâché mais aussi à tout ce qui est décrit antérieurement. Un véritable professionnel ce situe plutôt entre ( 500€ et 550€ ). A 1000€ et+ cela ne garantie pas forcément le professionnalisme mais plutôt une irrégularité du nombre de prestations annuelles. A ce prix vous ne payez pas uniquement sa prestation mais plutôt l’investissement de cette animateur . En clair à notre avis vous épongez son manque de contrat client signé.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°7</h2>
+        <p>
+            Votre DJ vient à votre rencontre avant l’événement pour planifier avec vous le déroulement de la soirée. Il est préférable pour vous que ce soit sur votre terrain c’est à dire à domicile. Son déplacement ne vous engage à rien alors dans un premier temps laisser le venir et vous démontrer son professionnalisme. Il est censé pouvoir vous orienter sur le bon déroulement d’une soirée compte tenu de son expérience du domaine. Si vous avez des doutes posez-lui toutes les questions décrites ci-dessus, vous serez rapidement fixé 😉.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°8</h2>
+        <p>
+            Le contrat. Nous abordons ici avec vous le critère le plus important. Si vous êtes un particulier nous allons vous expliquer certains détails très important à ne pas négliger dont vous n’avez pas forcément connaissance. Un DJ vous vend une prestation de service et non un bien. Il n’est donc pas commerçant et ne doit en aucun réclamer un acompte mais simplement des arrhes. C’est à dire qu’il ne peut exiger que 30% du montant total de la prestation. Si il vous réclame un acompte non seulement ça n’est pas légal mais en plus vous n’aurez aucun recours si il venait à se désister. Le contrat doit mentionner arrhes versées 30% article 293 du CGI non applicable. Si c’est le cas et que l’animateur DJ vous convient vous pouvez signer. A savoir que le simple fait d’avoir versé des arrhes engage le DJ si il venait à ne pas se présenter. Il vous serait redevable pénalement de deux fois le montant de sa prestation.
+        </p>
+    </article>
+    <article>
+        <h2>Critère n°9</h2>
+        <p>
+            La fiche entreprise et les avis clients. Forcément votre DJ professionnel à une fiche entreprise dans Google. Attention nous ne parlons pas ici des annonces d’annuaire ou les DJ effectue des paiements pour avoir des bonnes notes et de bons avis. Si vous voyez l’avis d’un annuaire sur un DJ et que celui-ci mentionne ( DJ en bronze, or ou platine) riez plutôt que vous faire piéger. Évidemment un DJ n’est pas fait de matériaux précieux. Du coup nous parlons bien d’aller consulter la fiche Google maps de votre futur DJ ou les anciens clients ont déposé leurs véritable avis sur votre DJ. Une fois sur la fiche entreprise vérifier l’ancienneté du DJ et le nombre d’avis. Si vous voyez 2 avis et que la note est de 5 sur 5 posez vous là question de qui à déposer un avis. Cela peut être le DJ lui même et son cousin lol 😁. Généralement il y a quelques dizaines d’avis et la note n’est jamais de 5 sur 5. La norme c’est plutôt entre 3,8 et 4,3. C’est comme pour votre restaurant préféré il n’aura jamais 5 sur 5 😉.
+        </p>
+    </article>
+</section>
