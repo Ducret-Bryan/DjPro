b546e63daf914bf85b82d7962aa1e1dd67cb87f6
Ducret Bryan <ducret.bryan@gmail.com>
2024-05-07 14:38:51 +0200

    hotfix(layout): bad copy-paste

--- app/src/view/layout.php
+++ app/src/view/layout.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="asset/styles/_globalStyle.css">
     <?php echo '<link rel="stylesheet" href="asset/styles/' . $page . '.css">' ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <div class="selectdiv ">
 </head>
 
 <body>
