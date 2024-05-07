990a5c585477ec19cb45b908beb2a6d5210a9eb2
Ducret Bryan <ducret.bryan@gmail.com>
2024-05-07 16:04:50 +0200

    feat: add nosDjs page

---
+++ app/src/controller/djsListController.php
@@ -0,0 +1,8 @@
+<?php
+
+function djsListController()
+{
+    $title = 'Djs Pro - Nos Djs';
+    $page = 'djsList';
+    include 'src/view/layout.php';
+}
---
+++ app/src/view/pages/djsList.php
@@ -0,0 +1,69 @@
+<h1>
+    Nos DJs
+</h1>
+<p id="subTitle">
+    Liste des DJs sélectionné par nos soins
+</p>
+
+<section>
+    <article>
+        <img class="avatar" src="asset/image/djs/default.jpg" alt="avatar" />
+        <div class="content">
+            <p class="name">Ducret Bryan</p>
+            <ul class="infos">
+                <li><span>Entreprise: </span>Djs Pro</li>
+                <li><span>Zone de travail: </span>Haute-marne</li>
+                <li><span>Type: </span>Anniversaire</li>
+                <li><span>Tél: </span>07 ** ** ** **</li>
+            </ul>
+            <p class="description">
+                Je suis un Dj Junior en bonne voie pour devenir un vrai professionnel !
+            </p>
+        </div>
+    </article>
+    <article>
+        <img class="avatar" src="asset/image/djs/default.jpg" alt="avatar" />
+        <div class="content">
+            <p class="name">Ducret Bryan</p>
+            <ul class="infos">
+                <li><span>Entreprise: </span>Djs Pro</li>
+                <li><span>Zone de travail: </span>Haute-marne</li>
+                <li><span>Type: </span>Anniversaire</li>
+                <li><span>Tél: </span>07 ** ** ** **</li>
+            </ul>
+            <p class="description">
+                Je suis un Dj Junior en bonne voie pour devenir un vrai professionnel !
+            </p>
+        </div>
+    </article>
+    <article>
+        <img class="avatar" src="asset/image/djs/default.jpg" alt="avatar" />
+        <div class="content">
+            <p class="name">Ducret Bryan</p>
+            <ul class="infos">
+                <li><span>Entreprise: </span>Djs Pro</li>
+                <li><span>Zone de travail: </span>Haute-marne</li>
+                <li><span>Type: </span>Anniversaire</li>
+                <li><span>Tél: </span>07 ** ** ** **</li>
+            </ul>
+            <p class="description">
+                Je suis un Dj Junior en bonne voie pour devenir un vrai professionnel !
+            </p>
+        </div>
+    </article>
+    <article>
+        <img class="avatar" src="asset/image/djs/default.jpg" alt="avatar" />
+        <div class="content">
+            <p class="name">Ducret Bryan</p>
+            <ul class="infos">
+                <li><span>Entreprise: </span>Djs Pro</li>
+                <li><span>Zone de travail: </span>Haute-marne</li>
+                <li><span>Type: </span>Anniversaire</li>
+                <li><span>Tél: </span>07 ** ** ** **</li>
+            </ul>
+            <p class="description">
+                Je suis un Dj Junior en bonne voie pour devenir un vrai professionnel !
+            </p>
+        </div>
+    </article>
+</section>
