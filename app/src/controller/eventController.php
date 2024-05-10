582199a7bce61fdaf7f0584afd2e316c822f9b8e
Ducret Bryan <ducret.bryan@gmail.com>
2024-05-10 12:58:07 +0200

    feat: add event page

---
+++ app/src/controller/eventController.php
@@ -0,0 +1,9 @@
+<?php
+
+function eventPage()
+{
+    $title = 'Djs Pro - Nos Événements';
+    $page = 'event';
+    $script = [];
+    include 'src/view/layout.php';
+}
---
+++ app/src/view/pages/event.php
@@ -0,0 +1,19 @@
+<h1>
+    Nos Événements
+</h1>
+<p id="subTitle">
+    Qu'est-ce que tu attends pour venir danser ?!
+</p>
+
+<section>
+    <article>
+        <div class="head">
+            <p class="title">Soirée 80' 90' 2000 </p>
+            <p class="date-location">25 Mai 2024 (Saint-Dizier)</p>
+        </div>
+        <p class="content">
+            Soirée dansante assurée par 3 DJ pro qui vont vous mettre le feu le temps d'une soirée. Au programme DJ Seb de l'alegra, DJ Mike Cotton club et DJ Bruce org. Viens pas pleurer t'a maman hihi
+        </p>
+        <p class="footer">Retrouver l'événement sur <a href="https://www.billetweb.fr/soiree-80-90-2000-25-mai-st-dizier" target="_blank" rel="noopener">billetweb.fr</a></p>
+    </article>
+</section>
